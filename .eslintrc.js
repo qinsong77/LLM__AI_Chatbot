4c0d006825cf272513422bc11b5f44f15dd8f7fa
notend <xyqinsong@163.com>
2024-03-25 23:03:22 +0800

    feat: add prettier and enhance eslint config

---
+++ .eslintrc.js
@@ -0,0 +1,22 @@
+/** @type {import("eslint").Linter.Config} */
+module.exports = {
+  plugins: ['simple-import-sort'],
+  extends: [
+    'next/core-web-vitals',
+    'plugin:tailwindcss/recommended',
+    'plugin:prettier/recommended',
+  ],
+  rules: {
+    'simple-import-sort/imports': 'error',
+    'simple-import-sort/exports': 'error',
+    'tailwindcss/no-custom-classname': 'off',
+    'tailwindcss/classnames-order': 'off',
+  },
+  overrides: [
+    {
+      files: ['**/*.test.ts', '**/*.test.tsx'],
+      extends: ['plugin:vitest/recommended', 'plugin:testing-library/react'],
+    },
+  ],
+  ignorePatterns: ['node_modules/', '.next/', 'public/'],
+}
