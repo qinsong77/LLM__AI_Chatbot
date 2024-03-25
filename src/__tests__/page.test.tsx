21237dc3d7e0e5c8b77d75559077ccf063a6021b
notend <xyqinsong@163.com>
2024-03-25 23:06:50 +0800

    style: lint fix

--- src/__tests__/page.test.tsx
+++ src/__tests__/page.test.tsx
@@ -1,28 +1,33 @@
-import { expect, describe, it } from 'vitest'
 import { render, screen, within } from '@testing-library/react'
-import Page from '../app/page'
 import userEvent from '@testing-library/user-event'
+import { describe, expect, it } from 'vitest'
+
+import Page from '../app/page'
 describe('Home component', () => {
-    it('renders correctly', () => {
-       render(<Page />);
-        expect(screen.getByText(/Get started by editing/i)).toBeInTheDocument();
-        expect(screen.getByAltText('Vercel Logo')).toHaveAttribute('src', '/vercel.svg');
-        expect(screen.getByText(/Docs/i)).toBeInTheDocument();
-    });
+  it('renders correctly', () => {
+    render(<Page />)
+    expect(screen.getByText(/Get started by editing/i)).toBeInTheDocument()
+    expect(screen.getByAltText('Vercel Logo')).toHaveAttribute(
+      'src',
+      '/vercel.svg',
+    )
+    expect(screen.getByText(/Docs/i)).toBeInTheDocument()
+  })
 
-    it('renders images correctly', () => {
-       render(<Page />);
-        const nextLogo = screen.getByAltText('Next.js Logo');
+  it('renders images correctly', () => {
+    render(<Page />)
+    const nextLogo = screen.getByAltText('Next.js Logo')
 
-        expect(nextLogo).toHaveAttribute('src', '/next.svg');
-        expect(nextLogo).toHaveAttribute('height', '37');
-    });
+    expect(nextLogo).toHaveAttribute('src', '/next.svg')
+    expect(nextLogo).toHaveAttribute('height', '37')
+  })
 
-    it('handles click events', () => {
-        const user = userEvent.setup()
-        render(<Page />);
-        const learnLink = screen.getByRole('link', { name: /Learn/i });
+  it('handles click events', () => {
+    const user = userEvent.setup()
+    render(<Page />)
+    const learnLink = screen.getByRole('link', { name: /Learn/i })
 
-        user.click(learnLink);
-    });
-});
+    expect(learnLink).toBeInTheDocument()
+    user.click(learnLink)
+  })
+})
