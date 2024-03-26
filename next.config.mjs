493334897194af04604e7b003da84f2da6a0d0e7
notend <xyqinsong@163.com>
2024-03-26 15:17:09 +0800

    build: bundle analyze

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,12 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {};
+import withBundleAnalyzer from '@next/bundle-analyzer'
 
-export default nextConfig;
+const bundleAnalyzer = withBundleAnalyzer({
+  enabled: process.env.ANALYZE === 'true',
+})
+
+/** @type {import('next').NextConfig} */
+const nextConfig = bundleAnalyzer({
+  reactStrictMode: true,
+})
+
+export default nextConfig
